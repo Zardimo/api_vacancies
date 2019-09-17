c9594caf56be0d6d2599d039c47256cd783eeb7b
Zardimo <vasyasapeggin@rambler.ru>
2019-09-17 17:00:47 +0500

    delete function, create import funtion from api_vacancies_hh, change names

--- api_vacancies_sj.py
+++ api_vacancies_sj.py
@@ -1,9 +1,10 @@
 import requests
 import os
+from api_vacancies_hh import get_salary
 
 
-def unload_vacancies_sj(language):
-    key_sj = os.getenv('TOKEN')
+def job_unloading_sj(language):
+    key_sj = os.getenv('SECRET_TOKEN_SJ')
     url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id' : key_sj
@@ -19,43 +20,24 @@ def unload_vacancies_sj(language):
     return response.json()
 
 
-def get_salary_RUB_sj(vacancies):
-    salary = 0
-    non_rub = 0
-    for payment in vacancies:
-        if payment['paymentrency'] == 'rub': 
-            if payment['payment_from'] and payment['payment_to']:
-                salary += payment['payment_from'] + payment['payment_to'] / 2
-            if not payment['payment_to']:
-                salary += payment['payment_from'] * 1.2
-            if not payment['payment_from']:
-                salary += payment['payment_to'] * 0.8
-        else:
-            non_rub += 1
-    return [salary, non_rub]
-
-
-def get_sj_base():
-    language_list = ['C++', 'Python', 'Java', 'Ruby', 'C', 'C#', 'JavaScript', 'PHP', 'Typescript', 'Objective-C', 'Scala', 'Swift', 'Go']
-    language_base = {}
-    for language in language_list:
-        vacancies_base = unload_vacancies_sj(language)
+def get_sj_base(site):
+    languages = ['C++', 'Python', 'Java', 'Ruby', 'C', 'C#', 'JavaScript', 'PHP', 'Typescript', 'Objective-C', 'Scala', 'Swift', 'Go']
+    Data_on_languages_for_table_sj = {}
+    for language in languages:
+        vacancies_base = job_unloading_sj(language)
         if vacancies_base['total'] > 0:
-            try:
-                processed = vacancies_base['total'] - get_salary_RUB_sj(vacancies_base['objects'])[1]
-            except ZeroDivisionError:
-                processed = vacancies_base['total']
-            salary = int(get_salary_RUB_sj(vacancies_base['objects'])[0] / processed)
+            salary_vacancies, processed = get_salary(vacancies_base['objects'], site)
+            salary = int(salary_vacancies / processed)
         else:
             processed = salary = 0
-        language_base.update(
+        Data_on_languages_for_table_sj.update(
         {language : {
             'vacancies_found' : vacancies_base['total'],
             'vacancies_processed' : processed,
             'vacancies_salary' : salary
             }
         })
-    return language_base
+    return Data_on_languages_for_table_sj
 
 
 if __name__ == '__main__':
